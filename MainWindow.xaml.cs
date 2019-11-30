f7fae9a516ef2fef6dbd1b4fbafa1f933eaac571
dmytropoznah <dmytropoznah>
2019-11-30 16:40:50 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Markov_Chain_Sentence_Generator
+{
+    public partial class MainWindow : Window
+    {
+        Markov mk = null;
+        string text = null;
+        List<int> number_of_words = new List<int>() {10, 20, 30, 40, 50};
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            numberWords_comboBox.ItemsSource = number_of_words;
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+            mk = null;  // ensure no old models are used with a new file
+            OpenFileDialog openFileDailog = new OpenFileDialog();
+            openFileDailog.FileName = ""; // Default file name
+            openFileDailog.DefaultExt = ".txt"; // Default file extension
+            openFileDailog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+            Nullable<bool> result = openFileDailog.ShowDialog();
+            if (result == true)
+            {
+                // Open document
+                string filepath = openFileDailog.FileName;
+                try
+                {
+                    text = File.ReadAllText(filepath);
+                    filepath_textBox.Text = filepath;
+                }
+                catch (IOException ioEx)
+                {
+                    MessageBox.Show("There was a problem reading the slected file. Error: " + ioEx.Message);
+                    text = null;
+                    filepath_textBox.Text = "";
+                }
+            }
+        }   // end SelectFile()
+
+        private void TrainModel(object sender, RoutedEventArgs e)
+        {
+            if (text == null)
+            {
+                MessageBox.Show("There is no text available for training.");
+                return;
+            }
+            mk = new Markov(text);
+            try
+            {
+                mk.Train();
+                MessageBox.Show("Successfully Trained Markov model. You can now generate sentences.");
+                trainingStatus_label.Content = "Trained";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                trainingStatus_label.Content = "Not Trained";
+            }
+        }   // end TrainModel()
+
+        private void GenerateText(object sender, RoutedEventArgs e)
+        {
+            generatedText_textBlock.Text = "";
+            if (mk == null)
+            {
+                MessageBox.Show("You must first select a text file and click train before generating text.");
+                return;
+            }
+            if ((string)trainingStatus_label.Content != "Trained")
+            {
+                MessageBox.Show("There was a problem during loading and training the model. Please try again.");
+                return;
+            }
+            try
+            {
+                generatedText_textBlock.Text = mk.GenerateText(length: (int)numberWords_comboBox.SelectedValue);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem with generating text. Error: " + ex.Message);
+            }
+        }   // end GenerateText()
+    }   // END MainWindow{}
+}
---
+++ Markov.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_Chain_Sentence_Generator
+{
+    class Markov
+    {
+        private string text;  
+        private Dictionary<string, Trigram> model;  
+        private bool trained;
+
+        static Random random = new Random();
+
+        public Markov(string text)
+        {
+            this.text = text;
+            model = new Dictionary<string, Trigram>();
+            this.trained = false;
+        }
+
+        public void Train()
+        {
+            this.trained = false;
+            text = CleanText(text);
+            string[] sentences = text.Split('.');
+            if (sentences.Length < 2) 
+            {
+                throw new FormatException("The text provided has no periods('.'). Please use a text with sentences seperated with periods.");
+            }
+            for (int i = 0; i != sentences.Length; i++)
+            {
+                string[] words = sentences[i].Split(' ');
+                if (words.Length < 3) { break; } 
+                for (int j = 0; j != words.Length - 2; j++)
+                {
+                    string index = words[j] + words[j + 1];
+                    if (!model.ContainsKey(index))
+                    {
+                        model[index] = new Trigram(words[j], words[j+1]);  
+                    }
+                    model[index].Add(words[j + 2]); 
+                }
+            }
+            trained = true;
+        }   
+
+        public string GenerateText(int length = 20)
+        {
+            if (!trained)
+            {
+                throw new Exception("The model has not been trained yet.");
+            }
+            List<string> keyList = new List<string>(model.Keys);  
+            List<string> sentence = new List<string>();  
+            string[] index = model[keyList[random.Next(keyList.Count)]].prefixWords; 
+            sentence.Add(index[0]); 
+            sentence.Add(index[1]);
+            for (int i = 1; i != length; i++)
+            {
+                index[0] = sentence[i - 1]; 
+                index[1] = sentence[i];
+                try
+                {  
+                    List<string> suffixes = model[index[0] + index[1]].suffixes;
+                    string choice = suffixes[random.Next(suffixes.Count)];
+                    sentence.Add(choice);  
+                }
+                catch (KeyNotFoundException)  
+                {
+                    index = model[keyList[random.Next(keyList.Count)]].prefixWords;
+                    sentence.Add(index[0]); 
+                    sentence.Add(index[1]);
+                }
+            }
+            return string.Join(" ", sentence);
+        }  
+
+        private string CleanText(string text)
+        {   
+            string cleaned = text.Replace("...", "");
+            cleaned = cleaned.Replace("\n", "");
+            return cleaned;
+        }   
+
+    }   
+}
